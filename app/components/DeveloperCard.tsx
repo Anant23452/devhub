87c95aec971ae594d2001eca35649945b08baeda
anantkpatel <imagine231216@gmail.com>
2026-08-23 21:54:45 +0530

    k

--- app/components/DeveloperCard.tsx
+++ app/components/DeveloperCard.tsx
@@ -14,8 +14,20 @@ export default function DeveloperCard({
 }:DeveloperCardProps){
     return(
         <article>
+            <h2>{developer.name}</h2>
+            <p>@{developer.username} </p>
+            <h3>{developer.role} </h3>
+            <p>{developer.bio} </p>
+            <div className="">
+                {developer.skills.map(skill)=>(
+                    <span key={skill} className="key">
+                        {skill}
+                        
 
-            
+                    </span>
+                )}
+            </div>
+
         </article>
     )
 }
