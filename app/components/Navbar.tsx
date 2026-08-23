d491af3e3fd4f8ff6f0e57604b20040d010345ea
anantkpatel <imagine231216@gmail.com>
2026-08-23 19:50:36 +0530

    oi

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Navbar(){
     return(
-        <nav className="bg-orange-500 flex justify-between items-center p-4">
+        <nav className="bg-orange-500 flex justify-between items-center p-4 fixed top-0 left-0 w-full z-50 transition-transform duration-300">
             <Link className="font-black" href="/">DevHum</Link>
             <div className="flex justify-evenly gap-4 px-4">
                 <Link href="/">Home</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
+   
+      <main className="flex  w-full h-screen  flex-col justify-center items-center  bg-white dark:bg-black sm:items-start">
+        <section className="flex  my-5 bg-blue-500 h-screen items-center flex-col justify-center">
+          <h2>DevHum</h2>
+        <h1>Learn.Build.Connect</h1>
+
+        <p>
+          A community where developers share knowledge,
+          discover talented developers, and find people
+          to build with.
+        </p>
+        <Link href="/developers" >Explore Developers</Link>
+        <Link href="/blog" >Read Dev Blog</Link>
+        </section>
        
       </main>
-    </div>
+  
   );
 }
