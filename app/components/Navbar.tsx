ce4ec0620d422d1c5598e7e61e39db7188993056
anantkpatel <imagine231216@gmail.com>
2026-08-23 19:22:59 +0530

    uh

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 export default function Navbar(){
     return(
         <nav className="bg-orange-500 flex justify-between items-center p-4">
-            <Link href="/">DevHum</Link>
-            <div className="flex">
+            <Link className="font-black" href="/">DevHum</Link>
+            <div className="flex justify-evenly gap-4 px-4">
                 <Link href="/">Home</Link>
                 <Link href="/blog">Blog</Link>
                 <Link href="/developers">Discover</Link>
