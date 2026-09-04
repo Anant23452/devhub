c3ca82e68e7d54fc23881966132eef5dc7c01791
anantkpatel <imagine231216@gmail.com>
2026-09-04 17:44:04 +0530

    usestate

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,7 +1,13 @@
+"use client"
+ import {useState,useEffect}from "react";
+    import { developers } from "@/data/developers";
 export default function ConnectionsPage(){
+   const [likedid,setLikedid]= useState<number[]>([]);
+   
+
     return (
         <div className="min-h-screen bg-slate-50 px-4 py-16 dark:bg-slate-950 sm:px-6 lg:px-8">
-            <h1 className="text-center text-xl">My connections</h1>
+            <h1 className="text-center text-4xl">My connections</h1>
            
         </div>
     )
