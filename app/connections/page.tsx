9624b393cd69fcc9db90be58c64436aae41b8689
anantkpatel <imagine231216@gmail.com>
2026-09-04 18:04:45 +0530

    l

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -4,6 +4,14 @@
 export default function ConnectionsPage(){
    const [likedid,setLikedid]= useState<number[]>([]);
    
+   useEffect(()=>{
+    const savedLikes= localStorage.getItem("likedDeveloper")
+    if(savedLikes){
+        setLikedid(JSON.parse(savedLikes))
+
+    }
+   },[])
+
 
     return (
         <div className="min-h-screen bg-slate-50 px-4 py-16 dark:bg-slate-950 sm:px-6 lg:px-8">
