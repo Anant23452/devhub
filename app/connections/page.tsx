ea07a829e9c49f3eb9a4c8e89e24e879f1d9e5f4
anantkpatel <imagine231216@gmail.com>
2026-09-04 17:24:16 +0530

    commit connecitn

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,7 +1,7 @@
 export default function ConnectionsPage(){
     return (
         <div className="min-h-screen bg-slate-50 px-4 py-16 dark:bg-slate-950 sm:px-6 lg:px-8">
-            <h1 className="text-center font-x">My connections</h1>
+            <h1 className="text-center text-xl">My connections</h1>
            
         </div>
     )
