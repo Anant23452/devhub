e18e7cdf28ef774f427ca9f350318e7952e05ec1
anantkpatel <imagine231216@gmail.com>
2026-09-05 18:06:41 +0530

    fix:type of developer

--- types/developer.ts
+++ types/developer.ts
@@ -1,8 +1,23 @@
 export type Developer = {
-    id:number;
-    name :string,
-    username:string;
-    role:string;
-    bio:string;
-    skills:string[];
-}
+  id: number;
+  name: string;
+  username: string;
+  role: string;
+  bio: string;
+  location: string;
+  skills: string[];
+
+  github: string;
+  linkedin: string;
+
+  interests: string[];
+
+  lookingFor: string;
+
+  projects: {
+    id: number;
+    title: string;
+    description: string;
+    tech: string[];
+  }[];
+};
