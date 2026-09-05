5a1a994a608c6de813d939838bd7bc3b4be88158
anantkpatel <imagine231216@gmail.com>
2026-09-05 17:10:59 +0530

    issue solve

--- app/developers/[username]/page.tsx
+++ app/developers/[username]/page.tsx
@@ -1,29 +1,43 @@
 import { developers } from "@/data/developers";
-type DeveloperPage={
-    params:Promise<
-    {
-        username:string;
-    }>
+
+type DeveloperProfilePageProps = {
+  params: Promise<{
+    username: string;
+  }>;
 };
-export default function DeveloperPage({params}:DeveloperPageProps){
-    const developer= developers.find(dev=>dev.username===params.username);
-    if(!developer){
-        return <h1>Developer not found</h1>
 
-    }
-    return (
-        <main  className=" mx-auto overflow-hidden rounded-xl text p-6 shadow-lg border border-gray-100 transition-all hover:shadow-xl">
-            <h1>{developer.username} </h1>
-            <p>{developer.name} </p>
-            <h2>{developer.bio} </h2>
-            {/* <p>{developer.location} </p> */}
-            <div>
-                {developer.skills.map((skill)=>(
-                    <span key ={skill}>{skill} </span>
-                ))}
-            </div>
+export default async function DeveloperProfilePage({
+  params,
+}: DeveloperProfilePageProps) {
+  const { username } = await params;
 
-        </main>
-    )
+  const developer = developers.find(
+    (dev) => dev.username === username
+  );
 
+  if (!developer) {
+    return <h1>Developer not found</h1>;
+  }
+
+  return (
+    <main className="w-full ">
+      <h1>{developer.name}</h1>
+
+      <p>@{developer.username}</p>
+
+      <h2>{developer.role}</h2>
+
+      <p>{developer.bio}</p>
+
+      <p>{developer.location}</p>
+
+      <div>
+        {developer.skills.map((skill) => (
+          <span key={skill}>
+            {skill}
+          </span>
+        ))}
+      </div>
+    </main>
+  );
 }
